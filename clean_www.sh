7a724e57f7530b0f12ec29918f4f3f07e432e108
darci khey <darcikhey27@gmail.com>
2016-09-11 21:32:10 -0700

    Added final file

---
+++ clean_www.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SOURCE="/home/dk/Documents/GitHub/ruby_scripts" # the location of my repo
+DEST="/home/dk/ruby/test_dest" #destination for webserver files
+
+cd $SOURCE
+
+echo "updating repo: executing git pull "
+git pull
+
+echo "\n finished with pulling files"
+
+echo "files in web server  $DEST  are"
+echo "-------------------"
+ls -l $DEST
+echo "\n\n"
+
+echo "Updating files now: "
+rm -rf $DEST*
+
+cp -R $SOURCE $DEST
+echo "----------------------------\n"
+echo "Done updating files"
+echo "\n\n"
+echo "Current files are: "
+ls -l $DEST
+
