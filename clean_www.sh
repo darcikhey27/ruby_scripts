712ddd48372eb34202b01bc04da1b88f5befcda5
darci khey <darcikhey27@gmail.com>
2016-09-12 20:25:26 -0700

    final

--- clean_www.sh
+++ clean_www.sh
@@ -5,20 +5,22 @@ DEST="/home/dk/ruby/test_dest" #destination for webserver files
 
 cd $SOURCE
 
-echo "updating repo: executing git pull "
+echo "updating repo: executing \ngit pull "
 git pull
 
 echo "\n finished with pulling files"
 
-echo "files in web server  $DEST  are"
+echo "files in web server location $DEST  are"
 echo "-------------------"
 ls -l $DEST
 echo "\n\n"
 
 echo "Updating files now: "
-rm -rf $DEST*
+echo "Removing old files"
+#rm -rf $DEST*
+echo "\n\n copying new files"
+#cp -R $SOURCE $DEST
 
-cp -R $SOURCE $DEST
 echo "----------------------------\n"
 echo "Done updating files"
 echo "\n\n"
